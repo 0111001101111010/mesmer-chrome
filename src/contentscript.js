6ce79e52a914459c674d7a8e0df2fd0cc418f120
Shane Creighton-Young <srcreigh@uwaterloo.ca>
2014-09-13 05:33:59 -0400

    highlight the prominent post in yellow

--- src/contentscript.js
+++ src/contentscript.js
@@ -1,8 +1,52 @@
 $(function() {
-  // we use this (probably very inefficient) method of removing this item,
-  // because FB often will inject new elements dynamically without reloading
-  // the page.
-  $('body').bind('DOMSubtreeModified', function() {
-    $('#pagelet_group_mall').remove();
-  });
+  $(window).on('scroll', colorProminentPost);
 });
+
+function posts() {
+  posts_query_string = '#pagelet_group_mall div[id^="mall_post"';
+  return $(posts_query_string);
+}
+
+function colorProminentPost() {
+
+  // Find the element that is most prominent on this page.
+  var candidate = null;
+  var candidatePercent = 0;
+  posts().each(function(index) {
+    var percent = getPercentVisible(this);
+    $(this).css('background-color', 'white');
+    console.log($(this).attr('id') + ': ' + percent);
+    if (percent > candidatePercent) {
+      candidate = $(this);
+      candidatePercent = percent;
+    }
+  });
+
+  // color the element with the given id a nice golden color
+  if (candidate) {
+    candidate.css('background-color', '#FEFCD7');
+  }
+}
+
+function getPercentVisible(obj) {
+  var postTop = $(obj).offset().top;
+  var postBottom = postTop + $(obj).height();
+
+  if (postBottom < $(window).scrollTop()) {
+    return 0;
+  } else if (postTop > $(window).scrollTop() + $(window).height()) {
+    return 0;
+  } else {
+    var postHeight = postBottom - postTop;
+
+    // subtract the parts that aren't visible
+    postHeight -= Math.max(0, $(window).scrollTop() - postTop);
+    postHeight -= Math.max(0, postBottom - ($(window).scrollTop() + $(window).height()));
+
+    return postHeight / $(window).height();
+  }
+}
+
+
+
+
