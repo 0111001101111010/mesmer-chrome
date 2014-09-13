46265f7ec6e8fc39720b2afb99e97190e636de69
Shane Creighton-Young <srcreigh@uwaterloo.ca>
2014-09-13 03:17:07 -0400

    fix for dynamicallly-added pages

--- src/contentscript.js
+++ src/contentscript.js
@@ -1,3 +1,8 @@
-$(document).ready(function() {
-  $('#pagelet_group_mall').remove()
+$(function() {
+  // we use this (probably very inefficient) method of removing this item,
+  // because FB often will inject new elements dynamically without reloading
+  // the page.
+  $('body').bind('DOMSubtreeModified', function() {
+    $('#pagelet_group_mall').remove();
+  });
 });
